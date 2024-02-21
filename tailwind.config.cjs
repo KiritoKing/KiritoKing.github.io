a65b7228abc2898d509a92df152efba9427933b5
KiritoKing <kiritoclzh@gmail.com>
2024-02-21 22:56:59 +0800

    chore(ui): 使用css方法优化代码显示 减少js配置

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,20 +2,6 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 const _ = require('lodash')
 
-// 消除Heading中Code样式的字号与行高
-const headingCodePreset = _.range(1, 6)
-	.map((i) => `h${i}`)
-	.map((h) => {
-		return {
-			[h]: {
-				code: {
-					fontSize: 'inherit',
-					lineHeight: 'inherit'
-				}
-			}
-		}
-	})
-
 module.exports = {
 	darkMode: 'class',
 	content: ['./src/**/*.{astro,html,js,md,mdx,ts}'],
@@ -29,20 +15,6 @@ module.exports = {
 			},
 			gridTemplateColumns: {
 				list: 'repeat(auto-fill, minmax(400px, max-content))'
-			},
-			typography: {
-				DEFAULT: {
-					css: _.merge(
-						{
-							code: {
-								color: '#4870ac',
-								padding: '2px 4px 2px',
-								margin: '2px'
-							}
-						},
-						...headingCodePreset
-					)
-				}
 			}
 		}
 	},
